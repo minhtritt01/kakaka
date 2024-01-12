0eab2a549e6e8252c46356a2adca94d763d4af01
Phan Tri <tri_phan@onskyhealth.com>
2024-01-12 14:30:22 +0700

    adda

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
