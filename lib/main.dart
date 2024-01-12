bab724ca373529a3fb7405502dc144075687cbfa
Phan Tri <tri_phan@onskyhealth.com>
2024-01-12 14:08:12 +0700

    d

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Dashbo'),
+      home: const MyHomePage(title: 'Dashboasada'),
     );
   }
 }
